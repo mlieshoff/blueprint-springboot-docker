1c80abd7ee137a463613df09f084d6fe64a0889e
mlieshoff <michael.lieshoff@init.de>
2025-02-07 13:16:35 +0100

    Fix typos

--- src/main/java/com/mlieshoff/blueprint/dao/ExampleDaoDto.java
+++ src/main/java/com/mlieshoff/blueprint/dao/ExampleDaoDto.java
@@ -4,6 +4,6 @@ package com.mlieshoff.blueprint.dao;
  * This record represents an example DTO.
  *
  * @param id ID of the example
- * @param value valuf of the example
+ * @param value value of the example
  */
 public record ExampleDaoDto(Long id, String value) {}
